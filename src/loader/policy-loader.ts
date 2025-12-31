d50d5e95e57c62b7bb992a8a5b6a4998c5fea1e1
Lunix <lunix@ryzespace.com>
2025-12-31 01:02:04 +0100

    ,
    ,

--- src/loader/policy-loader.ts
+++ src/loader/policy-loader.ts
@@ -4,7 +4,7 @@ import yaml from "js-yaml";
 import { log } from "../utils/logger";
 import { Policy, LabelPolicy, createLabelEntity, LabelEntity } from "../engine/types/labels";
 
-const DEFAULT_POLICIES_DIR = path.join(__dirname, "dist/default-policies");
+const DEFAULT_POLICIES_DIR = path.join(process.env.GITHUB_ACTION_PATH || __dirname, "default-policies");
 
 
 async function loadPolicyFile(fileName: string): Promise<Policy> {
